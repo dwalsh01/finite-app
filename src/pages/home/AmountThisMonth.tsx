be673e139591de21c10972ba36cec1d23d462b7a
dwalsh01 <daraghwalsh97@gmail.com>
2020-01-05 15:40:11 +0000

    Updated the sortExpenses.ts functions and removed old sort function. Will be making more changes to this going forward so there's a need for this branch. Will also implement the UI changes here.

--- src/pages/home/AmountThisMonth.tsx
+++ src/pages/home/AmountThisMonth.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import GET_THIS_MONTH_EXPENSES from '../../graphql/ExpensesThisMonth';
 import { GetExpensesThisMonth } from '../../types/GetExpensesThisMonth';
-import sortExpenses, { sortDates, totalForTheMonth } from '../../utils/sortExpenses';
+import { sortDates, totalForTheMonth } from '../../utils/sortExpenses';
 
 const AmountThisMonth: React.FC = () => {
   const { data, loading } = useQuery<GetExpensesThisMonth>(GET_THIS_MONTH_EXPENSES);
@@ -10,7 +10,6 @@ const AmountThisMonth: React.FC = () => {
     return null;
   }
   if (data?.getExpenses?.expensesThisMonth) {
-    console.log('SortExpenses(): ', sortExpenses(data.getExpenses.expensesThisMonth));
     console.log('sortDates(): ', sortDates(data?.getExpenses.expensesThisMonth));
     console.log('totalForTheMonth(): ', totalForTheMonth(data?.getExpenses.expensesThisMonth));
   }
--- src/utils/sortExpenses.ts
+++ src/utils/sortExpenses.ts
@@ -1,12 +1,6 @@
 import _ from 'lodash';
 import { GetExpenses_me_expenses } from '../types/GetExpenses';
 
-interface Final {
-  month: string;
-  amount: number;
-  dateOfExpense: string;
-}
-
 const MONTH_NAMES = [
   'January',
   'February',
@@ -22,45 +16,6 @@ const MONTH_NAMES = [
   'December',
 ];
 
-// OLD
-// ----------------------------------------------------------------------
-// TODO: make this function take a second argument which will sort based on month/date specific
-// TODO: clean this up
-const sortExpenses = (expenses: GetExpenses_me_expenses[]) => {
-  // this is already taking in this months expenses (where called at the moment)
-  // group by date
-  const res = _.mapValues(_.groupBy(expenses, 'dateOfExpense'));
-  // eslint-disable-next-line
-  const ret: any = [];
-  let month = '';
-  // dates
-  const keys = Object.keys(res);
-  const final: Final = { month, amount: 0, dateOfExpense: '' };
-  keys.forEach(key => {
-    const objs = res[key];
-    objs.forEach((item: GetExpenses_me_expenses) => {
-      if (month === '') {
-        month = MONTH_NAMES[new Date(item.dateOfExpense).getMonth()];
-      }
-      final.amount += item.amount;
-      final.month = MONTH_NAMES[new Date(item.dateOfExpense).getMonth()];
-      final.dateOfExpense = item.dateOfExpense;
-      ret.push(final);
-    });
-  });
-
-  ret.sort((a: Final, b: Final) => {
-    return new Date(a.dateOfExpense).getTime() - new Date(b.dateOfExpense).getTime();
-  });
-  return {
-    sortedThisMonth: res,
-    month,
-    totalMonth: final.amount,
-  };
-};
-
-// --------------------------------------------------
-
 export function groupDatesAndKeys(expenses: GetExpenses_me_expenses[]) {
   const groupByDate = _.mapValues(_.groupBy(expenses, 'dateOfExpense'));
   const dates = Object.keys(groupByDate);
@@ -68,13 +23,12 @@ export function groupDatesAndKeys(expenses: GetExpenses_me_expenses[]) {
 }
 
 // the dates that are passed are grouped by day and sorted in order
-export function sortDates(expenses: GetExpenses_me_expenses[]) {
+function sortDates(expenses: GetExpenses_me_expenses[]) {
   const groupByDate = _.mapValues(_.groupBy(expenses, 'dateOfExpense'));
   const dates = Object.keys(groupByDate);
   dates.sort((a: string, b: string) => {
     return new Date(a).getTime() - new Date(b).getTime();
   });
-  // eslint-ignore-next-line
   const sortedDates: Record<string, GetExpenses_me_expenses[]> = {};
   dates.forEach(date => {
     sortedDates[date] = groupByDate[date];
@@ -87,7 +41,8 @@ interface Total {
   amount: number;
   month: string;
 }
-export function totalForTheMonth(expenses: GetExpenses_me_expenses[]) {
+// TODO: make this a function that takes a second parameter which is month, week, quarter, year
+function totalForTheMonth(expenses: GetExpenses_me_expenses[]) {
   const { groupByDate, dates } = groupDatesAndKeys(expenses);
   const total: Total = { amount: 0, month: '' };
   dates.forEach(date => {
@@ -102,4 +57,4 @@ export function totalForTheMonth(expenses: GetExpenses_me_expenses[]) {
   return total;
 }
 
-export default sortExpenses;
+export { totalForTheMonth, sortDates };
